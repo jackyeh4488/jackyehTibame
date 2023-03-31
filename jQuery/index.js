525056fd6a6620ca6072376883606105ef8c0c4e
jackyeh4488 <jackyeh4488@gmail.com>
2023-03-31 09:07:09 +0800

    更新

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    // 這邊是滑鼠滾輪事件
     $(window).scroll(function(){
         if ($(window).scrollTop() > 300 ) {
             $('.to-top').removeClass('hide')
@@ -6,16 +7,23 @@ $(document).ready(function(){
             $('.to-top').addClass('hide')
         }
     });
-
+    // 點擊後回到最上面
     $('.to-top').click(function(event){
         event.preventDefault();
         $('html,body').animate({
             scrollTop: 0
         }, 700);
     });
-
+    // 點擊後側邊欄會出現
     $('.aside-btn').click(function(event){
         $('body').toggleClass('open');
         // alert("測試測試測試");
+        $('.click-to-hide').toggleClass('open')
+        // $(body).off('mousewheel DOMMouseScroll')
+    });
+
+    // 燈箱效果
+    $('.click-to-hide').click(function(event){
+        $('body').removeClass('open')
     });
 });
